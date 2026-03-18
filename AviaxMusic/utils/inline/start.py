cd650199441e293448c2c0cafca5bff40aedd0d9
˹ ꜱᴘɪᴄʏ ᴄᴏᴅᴇʀꜱ ˼ <im_maxim@inbox.ru>
2026-03-18 09:22:30 +0530

    Add files via upload

--- AviaxMusic/utils/inline/start.py
+++ AviaxMusic/utils/inline/start.py
@@ -1,4 +1,5 @@
 from pyrogram.types import InlineKeyboardButton
+from pyrogram.enums import ButtonStyle
 import config
 from AviaxMusic import app
 
@@ -21,16 +22,17 @@ def private_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
+                style=ButtonStyle.DANGER,
             )
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID, style=ButtonStyle.SUCCESS),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP, style=ButtonStyle.SUCCESS),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/SpIcYxNeTwOrK"),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL, style=ButtonStyle.SUCCESS),
+            InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/ll_ROYAL_ABOUT_ll", style=ButtonStyle.SUCCESS),
         ],
     ]
     return buttons
