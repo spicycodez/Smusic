20e0522e7f9e852d4a3ebf9cf49eaf6329cf1179
˹ ꜱᴘɪᴄʏ ᴄᴏᴅᴇʀꜱ ˼ <im_maxim@inbox.ru>
2026-03-27 17:32:28 +0530

    Update start.py

--- AviaxMusic/utils/inline/start.py
+++ AviaxMusic/utils/inline/start.py
@@ -21,24 +21,18 @@ def private_panel(_):
         [
             InlineKeyboardButton(
                 text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
-                style=ButtonStyle.DANGER,
+                url=f"https://t.me/{app.username}?startgroup=true"
             )
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+        #[InlineKeyboardButton(text="𝖠𝖯𝖨 𝖢𝗈𝗇𝗌𝗈𝗅𝖾",callback_data="api_console")],
         [
-            InlineKeyboardButton(
-                text="𝖠𝖯𝖨 𝖢𝗈𝗇𝗌𝗈𝗅𝖾",
-                callback_data="api_console"
-            )
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
-        [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID, style=ButtonStyle.SUCCESS),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP, style=ButtonStyle.SUCCESS),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL, style=ButtonStyle.SUCCESS),
-            InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/SpIcYxNeTwOrK", style=ButtonStyle.SUCCESS),
-        ],
+        #[
+          #  InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+          #  InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/SpIcYxNeTwOrK"),
+       # ],
     ]
     return buttons
