7b2f818fdfeebfd2420684bf47717adc005b6ab5
˹ ꜱᴘɪᴄʏ ᴄᴏᴅᴇʀꜱ ˼ <im_maxim@inbox.ru>
2026-04-27 23:22:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,9 +30,9 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-API_URL = getenv("API_URL", 'https://api.nexgenbots.xyz') #youtube song url
-VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.nexgenbots.xyz')
-API_KEY = getenv("API_KEY", None) # youtube song api key, generate free key or buy paid plan from https://console.nexgenbots.xyz
+API_URL = getenv("API_URL", 'https://tgapi.xbitcode.com') #youtube song url
+VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://tgapi.xbitcode.com')
+API_KEY = getenv("API_KEY", "xbit_issQbNpQKwoZaRVBQtEogF1kcbVorV4I") # youtube song api key, generate free key or buy paid plan from https://console.nexgenbots.xyz
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
